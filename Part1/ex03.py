dbeda4bd68d6bd32fab1493908066756874dcfdd
Xiaowheyy <xiaowhey07@gmail.com>
2025-07-21 05:58:04 +0000

    work done for day1

---
+++ Part1/ex03.py
@@ -0,0 +1,17 @@
+# Original text
+string1 = 'MaRviN THE bOt'
+
+#convert to lowercase
+string_lower = 'eat'
+
+#convert to uppercase 
+string_upper = 'eat'
+
+#remove spaces
+string_nospace = 'eat'
+
+# Print results
+print(string1)
+print(string_lower)
+print(string_upper)
+print(string_nospace)
---
+++ Part1/ex05.py
@@ -0,0 +1,10 @@
+# Word variables
+word1 = 'car '
+word2 = 'exceeded '
+text1 = 'speed limit '
+word3 = 'the '
+
+# TODO: Combine the words into one sentence and display on screen
+sentence = word3 + word1 + word2 + word1 
+
+print(sentence)
